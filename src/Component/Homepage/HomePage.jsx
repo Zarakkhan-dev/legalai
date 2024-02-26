2bd940f95f017d49ffa711ce806f21e9b0d7d6ed
Zarak Khan <zarakkhan23630@gmail.com>
2024-02-26 15:05:46 +0500

    HomePage 1 section complete

--- src/Component/Homepage/HomePage.jsx
+++ src/Component/Homepage/HomePage.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 import header_pic from "../../../public/Picture/thom-holmes-J2e34-1CVVs-unsplash.jpg";
 import axios from 'axios';
+import Punjab_Bar_Council from "../../../public/Picture/Punjab_Bar_Council.png"
+import Tuf_logo from "../../../public/Picture/Tuf_logo.png";
+import Home_Page_Detail_1 from "../../../public/Picture/Home-Page-Detail-1.png"
 const HomePage = () => {
 
   const [email,Setemail] = useState("");
@@ -17,8 +20,8 @@ const HomePage = () => {
   }
   return (
     <>
-      <header>
-      <div className="w-screen">
+      <header className='HomePage '>
+      <section>
 
 <div className="relative mx-auto flex flex-col px-4 sm:max-w-xl md:max-w-screen-xl md:flex-row">
   
@@ -34,16 +37,23 @@ const HomePage = () => {
         <p className="text-base text-gray-700 md:text-lg">The software comes with AI management features to access data bases, assign tasks and track progress of various activities related to a case.</p>
       </div>
 
-      <div className="header-email-box  flex ">
-        <span className='border-[#3292A30] border-[1px] rounded-3xl py-1 flex'>
+      <div className="header-email-box  flex  ">
+        <span className='border-[#3292A30] border-[1px] rounded-3xl py-1 flex ml-[5vh]'>
         <input type="email" name="email" id="" placeholder='Email Address' className='ml-5 w-[40vh] outline-none' onChange={(e)=>Setemail(e.target.value)} value={email}/>
         <div className="icon bg-[#2C60EA] rounded-full py-2 px-3  mx-[5px] cursor-pointer" onClick={sendMail}>
         <i className="fa-solid fa-paper-plane " style={{color:"white"}}></i>
         </div>
         </span>
       </div>
-      <div className="flex items-center bg-slate-500">
-
+      <div className="flex items-center mt-6 gap-3 ml-9">
+        <div className="stars flex gap-2">
+        <i className="fa-solid fa-star fa-xl" style={{color:'#F5A620'}}></i>
+        <i className="fa-solid fa-star fa-xl" style={{color:'#F5A620'}}></i>
+        <i className="fa-solid fa-star fa-xl" style={{color:'#F5A620'}}></i>
+        <i className="fa-solid fa-star fa-xl" style={{color:'#F5A620'}}></i>
+        <i className="fa-solid fa-star fa-xl" style={{color:'#F5A620'}}></i>
+        </div>
+        <p className=' font-normal text-lg'>Rated 5 star by satisfied clients</p>
       </div>
     </div>
   </div>
@@ -51,19 +61,57 @@ const HomePage = () => {
   <div className="header-flex-2 flex h-full w-full space-x-3  px-2 md:justify-end ">
   
    
-    <div className="Header-image  w-[60%] h-[50vh] flex-col space-y-3 md:mt-20 lg:flex ">
+    <div className="Header-image  w-[60%] h-[50vh] flex-col space-y-0 md:mt-20  ">
      
       <Image src={header_pic} width={500} height={500} alt='pic' className=' w-[100%] h-[100%] rounded-[6vh] ' />
     </div>
   </div>
 
 </div>
-</div>
+</section>
       </header>
 
       <div className="partener-section mt-[15vh]">
-        <h1 className='text-center text-2xl font-semibold'>Partnered with over 50 bar associations</h1>
+        <h1 className='text-center text-2xl font-semibold'>Partnered with over 2 bar associations</h1>
+         <div className="Parteners w-[80%] mx-auto flex justify-center gap-5 mt-10 items-center">
+          <Image src={Punjab_Bar_Council} width={100} height={100}/>
+          <Image src={Tuf_logo} width={150} height={150}/>
+         </div>
       </div>
+     <section className='Detail-Section-1  '>
+      <div className="Detail-Section-Image-1 ">
+     <Image  src={Home_Page_Detail_1} width={1500} height={1500} className='w-[100%] h-[100%]'/>
+      </div>
+      <div className="Detail-Section-content flex flex-col  place-items-start w-[60%] mt-10">
+        <h1 className='text-[#F29F66]'>Fast Track</h1>
+        <h1 className=" text-4xl">Grow faster with AI Legal Assistant.</h1>
+        <p className='my-3'>Get a 360-degree view of your cases. Attorneys and staff can track leads, access data bases and help from AI based legal assistant by just signing</p>
+        <ul >
+          <li>Customize web based client intake forms for any area of your practice.</li>
+        <li>Handle the client cases easily with AI assistent.</li>
+        <li>Easily automate the procedure with already available data bases stored in system.</li>
+        </ul>
+        <button className='mt-4 py-3 px-10 bg-[#2C60EA] text-white font-semibold rounded-full'>See Product Description </button>
+      </div>
+     </section>
+
+     <section className='Detail-Section-2 '>
+      <div className="Detail-Section-2-Content">
+      <h1 className='text-[#F29F66]'>Easily Generate</h1>
+        <h1 className=" text-4xl">Stay organised with AI Legal Assistant.</h1>
+        <p className='my-3'>From calendering and communications to document management and case reporting, keep track of every detail without falling behind.</p>
+        <ul >
+          <li>Stay infront of important deadlines with one
+centerlized calender.</li>
+        <li>Easily generate, edit, and access data bases.</li>
+        <li>Provides assistance in solving cases.</li>
+        </ul>
+        <button className='mt-4 py-3 px-10 bg-[#2C60EA] text-white font-semibold rounded-full'>See Product Description </button>
+      </div>
+      <div className="Detail-Section-2-Image">
+
+      </div>
+     </section>
     </>
   )
 }
